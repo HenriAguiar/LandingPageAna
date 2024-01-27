aa5030bfebacea6501451939c3f9179bbd84c808
HenriAguiar <aguiar.henri1@gmail.com>
2024-01-27 00:47:48 -0300

    Fix: Altera layout da capa em telas lg

--- src/app/page.js
+++ src/app/page.js
@@ -2,78 +2,141 @@
 export default function Page() {
   return (
     <>
-      <div className=" fundoLilas">
+      <div className="fundoBranco">
         <div className="container containerCapa">
-          <div className="row row-cols-1 row-cols-lg-2">
-            <div className="col">
-              <div className="d-flex justify-content-start align-items-center">
-                <h className="tituloSecao">CURSO ANA BANANA, O MELHOR CURSO DE BANANAGEM</h>
-              </div>
-              <div className="d-flex paragrafoSecao justify-content-start align-items-center pt-4 pb-4">
-                <h>O curso ana banana é o curso da ana banana que te ensina a ser uma banana</h>
-              </div>
-              
-              <div className="ratio ratio-16x9">
-                <iframe
-                  className="videoCapa d-flex align-items-center justify-content-center"
-                  src='https://www.youtube.com/embed/6uo3Nj0sfSw'
-                  title="YouTube video player"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen
-                ></iframe>
-              </div>
-              <div className="d-flex justify-content-center aling-items-center  pt-3 pb-3">
-                <a href="https://khaledkzy.github.io/pixel-vh-vw-converter/" className="link">
-                  <div className="botao">
-                    <h className="textoBotao">Seja uma banana</h>
-                  </div>
-                </a>
-              </div>
-              <div className="row row-cols-3 pt-4">
-                <div className="col">
-                  <div className="row row-cols-1">
+          <div className="subContainer">
+            <div className="row row-cols-1 row-cols-xl-2 gx-5">
+              <div className="col">
+                <div className="d-flex justify-content-start align-items-center">
+                  <h className="tituloSecao">CURSO ANA BANANA, O MELHOR CURSO DE BANANAGEM, SE TORNE UMA BANANA EM ATÉ 30 DIAS</h>
+                </div>
+                <div className="d-flex paragrafoSecao justify-content-start align-items-center pt-4 pb-5">
+                  <h>O curso ana banana é o curso da ana banana que te ensina a ser uma banana em até 30 dias, sem complicação, com um plano claro para você obter os melhores resultados</h>
+                </div>
+                <div className="d-none d-xl-block pt-1">
+                  <div className="row row-cols-3 pt-4">
                     <div className="col">
-                      <div className="d-flex align-items-center justify-content-center">
-                        <img src="/images/check.svg" />
+                      <div className="row row-cols-1">
+                        <div className="col">
+                          <div className="d-flex align-items-center justify-content-center">
+                            <img src="/images/check.svg" />
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="d-flex align-items-center justify-content-center">
+                            <h className="legendaIcone">
+                              Acesso imediato
+                            </h>
+                          </div>
+                        </div>
                       </div>
                     </div>
                     <div className="col">
-                      <div className="d-flex align-items-center justify-content-center">
-                        <h className="legendaIcone">
-                          Acesso imediato
-                        </h>
+                      <div className="row row-cols-1">
+                        <div className="col">
+                          <div className="d-flex align-items-center justify-content-center">
+                            <img src="/images/chat.svg" />
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="d-flex align-items-center justify-content-center">
+                            <h className="legendaIcone">
+                              Suporte à dúvidas
+                            </h>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="row row-cols-1">
+                        <div className="col">
+                          <div className="d-flex align-items-center justify-content-center">
+                            <img src="/images/lock.svg" />
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="d-flex align-items-center justify-content-center">
+                            <h className="legendaIcone">
+                              Garantia de 7 dias
+                            </h>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="col">
-                  <div className="row row-cols-1">
-                    <div className="col">
-                      <div className="d-flex align-items-center justify-content-center">
-                        <img src="/images/chat.svg" />
+
+              </div>
+
+              <div className="col">
+                <div className="align-items-center">
+                  <div className="ratio ratio-16x9">
+                    <iframe
+                      className="videoCapa d-flex align-items-center justify-content-center"
+                      src='https://www.youtube.com/embed/6uo3Nj0sfSw'
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                  <div className="d-flex justify-content-center aling-items-center  pt-3 pb-3">
+                    <a href="https://khaledkzy.github.io/pixel-vh-vw-converter/" className="link">
+                      <div className="botao">
+                        <h className="textoBotao">Seja uma banana</h>
                       </div>
-                    </div>
-                    <div className="col">
-                      <div className="d-flex align-items-center justify-content-center">
-                        <h className="legendaIcone">
-                          Suporte à dúvidas
-                        </h>
+                    </a>
+                  </div>
+                </div>
+
+              </div>
+
+              <div className="col d-none d-sm-block d-xl-none pb-5">
+                <div className="row row-cols-3 pt-4">
+                  <div className="col">
+                    <div className="row row-cols-1">
+                      <div className="col">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <img src="/images/check.svg" />
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <h className="legendaIcone">
+                            Acesso imediato
+                          </h>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="col">
-                  <div className="row row-cols-1">
-                    <div className="col">
-                      <div className="d-flex align-items-center justify-content-center">
-                        <img src="/images/lock.svg" />
+                  <div className="col">
+                    <div className="row row-cols-1">
+                      <div className="col">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <img src="/images/chat.svg" />
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <h className="legendaIcone">
+                            Suporte à dúvidas
+                          </h>
+                        </div>
                       </div>
                     </div>
-                    <div className="col">
-                      <div className="d-flex align-items-center justify-content-center">
-                        <h className="legendaIcone">
-                          Garantia de 7 dias
-                        </h>
+                  </div>
+                  <div className="col">
+                    <div className="row row-cols-1">
+                      <div className="col">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <img src="/images/lock.svg" />
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <h className="legendaIcone">
+                            Garantia de 7 dias
+                          </h>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -82,6 +145,7 @@ export default function Page() {
             </div>
           </div>
         </div>
+
       </div>
     </>
   )
