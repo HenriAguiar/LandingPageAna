546ca3c274b3b9a31344922cb5dbfdc8a7be4709
HenriAguiar <aguiar.henri1@gmail.com>
2024-02-04 22:38:43 -0300

    Fix: Cria instancia local da fonte SDPro

--- src/app/components/CardR.js
+++ src/app/components/CardR.js
@@ -1,4 +1,9 @@
+import SFPro from 'next/font/local'
 
+const apple = SFPro({
+    src: './SFPro.ttf',
+    display: 'swap',
+  })
 export default function CardR() {
     return (
         <div className="d-flex align-items-center justify-content-center divcar">
@@ -10,9 +15,9 @@ export default function CardR() {
                     </div>
                     <div className="balaocar">
                         <div className="textocar">
-                            Só queria dizer o quanto seu curso de maquiagem mudou minha vida. Antes, eu me sentia perdida, mas suas aulas simplificaram tudo. Aprendi a realçar minha beleza de forma natural e ganhei confiança com seus truques.
+                            <p className={apple.className}> Só queria dizer o quanto seu curso de maquiagem mudou minha vida. Antes, eu me sentia perdida, mas suas aulas simplificaram tudo. Aprendi a realçar minha beleza de forma natural e ganhei confiança com seus truques.
                             A evolução foi incrível. Seus conselhos sobre produtos e técnicas fizeram toda a diferença. E a comunidade que você criou em torno do curso é inspiradora.
-                            Obrigada por compartilhar seu conhecimento de maneira acessível e motivadora. Estou ansiosa para continuar aprendendo.
+                            Obrigada por compartilhar seu conhecimento de maneira acessível e motivadora. Estou ansiosa para continuar aprendendo.</p>
                         </div>
                     </div>
                 </div>
